41d232bc5a8a87fdb6697670f209b6ad8ac984aa
Eduardo445 <esoto-rodriguez@csumb.edu>
2020-10-01 01:20:23 +0000

    Starting to add the database

--- app.js
+++ app.js
@@ -1,43 +1,61 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var stylus = require('stylus');
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public")); // access images, css, js
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// const MongoClient = require("mongodb").MongoClient;
 
-var app = express();
+// Connect to mongodb
+const uri = 'mongodb+srv://Esoto1290:CSTwebstore1900@cst438.vwxeq.mongodb.net/WebStore?retryWrites=true&w=majority';
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => console.log('connected to db'))
+    .catch((err) => console.log(err));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// const client = new MongoClient(process.env.ATLAS_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(stylus.middleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
+// client.connect(err => {
+//   const collection = client.db("WebStore").collection("Customers");
+//   // perform actions on the collection object
+//   client.close();
+// });
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// client.connect().then(result => {
+//     const database = client.db("WebStore");
+//     const collection = database.collection("Customers");
+//     console.log(result);
+// }, error => {
+//     console.error(error);
+// });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// (async () => {
+//     try {
+//         await client.connect();
+//         const database = client.db("WebStore");
+//         const collection = database.collection("Customers");
+//         const result = await collection.insertOne({
+//             "firstName": "Eduardo",
+//             "lastName" : "Soto",
+//             "username" : "Testing",
+//             "password" : "Admin!"
+//         });
+//         console.log(result.insertId);
+//         client.close();
+//     } catch (err) {
+//         console.error(err);
+//     }
+    
+// })();
+
+// const request = require('request');
+
+app.get("/", function(req, res){
+    // res.render("index.ejs");
+    res.send("It works!");
+});//root
+
+//running server
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Express server is running...");
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
